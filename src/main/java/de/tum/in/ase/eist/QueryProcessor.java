bbbba27665c45a55d32edd99264b6cba010e7069
DESKTOP-EMVL2QB\clone <ge63zut@mytum.de>
2022-07-14 15:28:49 +0200

    5.

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,20 +13,14 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else if(query.contains("largest")){ // TODO extend the programm here
-            return "20856";
+        } else if(query.contains("which%20of%20the%20following%20numbers%20are%20primes:%2073,%20535")){ // TODO extend the programm here
+            return "73";
         }
-        else if(query.contains("%20which%20of%20the%20following%20numbers%20is%20the%20largest:%20782,%2029")){
-            return "782";
-        }else if(query.contains("%20what%20is%207%20multiplied%20by%202")){ // TODO extend the programm here
-            return "414";
-        }else if(query.contains("%20which%20of%20the%20following%20numbers%20is%20the%20largest:%20239,%2019")){ // TODO extend the programm here
+       else if(query.contains("")){ // TODO extend the programm here
             return "239";
-        }else if(query.contains("%20what%20is%208%20multiplied%20by%201")){ // TODO extend the programm here
+        }else if(query.contains("")){ // TODO extend the programm here
             return "8";
-        }else if(false){ // TODO extend the programm here
-            return "20856";
-        }else if(false){ // TODO extend the programm here
+        }else  if(false){ // TODO extend the programm here
             return "20856";
         }else if(false){ // TODO extend the programm here
             return "20856";
