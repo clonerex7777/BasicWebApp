6505df6d6c8ec215917bb718cd9b8123065a1fbd
DESKTOP-EMVL2QB\clone <ge63zut@mytum.de>
2022-07-14 15:25:09 +0200

    3.

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -20,8 +20,8 @@ public class QueryProcessor {
             return "782";
         }else if(query.contains("%20what%20is%207%20multiplied%20by%202")){ // TODO extend the programm here
             return "414";
-        }else if(false){ // TODO extend the programm here
-            return "20856";
+        }else if(query.contains("%20which%20of%20the%20following%20numbers%20is%20the%20largest:%20239,%2019")){ // TODO extend the programm here
+            return "239";
         }else if(false){ // TODO extend the programm here
             return "20856";
         }else if(false){ // TODO extend the programm here
