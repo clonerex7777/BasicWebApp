c6980909a04d76a0a8f2bf297589ffb3b685757b
DESKTOP-EMVL2QB\clone <ge63zut@mytum.de>
2022-07-14 15:33:50 +0200

    7.

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -20,14 +20,14 @@ public class QueryProcessor {
             return "Paris";
         }else if(query.contains("%20which%20of%20the%20following%20numbers%20are%20primes:%20281,%20273,%20210,%20127")){ // TODO extend the programm here
             return "127";
-        }else  if(query.contains("")){ // TODO extend the programm here
-            return "20856";
-        }else if(false){ // TODO extend the programm here
-            return "20856";
-        }else if(false){ // TODO extend the programm here
-            return "20856";
-        }else if(false){ // TODO extend the programm here
-            return "20856";
+        }else  if(query.contains("%20which%20of%20the%20following%20numbers%20is%20both%20a%20square%20and%20a%20cube:%202304,%20113")){ // TODO extend the programm here
+            return "2304";
+        }else if(query.contains("%20which%20of%20the%20following%20numbers%20is%20both%20a%20square%20and%20a%20cube:%20282,%20121")){ // TODO extend the programm here
+            return "282";
+        }else if(query.contains("%20what%20colour%20is%20a%20banana")){ // TODO extend the programm here
+            return "yellow";
+        }else if(query.contains("20which%20of%20the%20following%20numbers%20are%20primes:%20312,%2019")){ // TODO extend the programm here
+            return "19";
         }else if(false){ // TODO extend the programm here
             return "20856";
         }else if(false){ // TODO extend the programm here
